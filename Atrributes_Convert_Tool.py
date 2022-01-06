39023f570607711ac81ebc3982527830b063c8c3
Takeshi Chō <takeshi_cho@outlook.com>
2022-01-07 00:22:03 +0800

    Update Code

--- Atrributes_Convert_Tool.py
+++ Atrributes_Convert_Tool.py
@@ -1,21 +1,21 @@
 bl_info = {
-    "name": "Atrributes Convert Tool",
+    "name": "Attributes Convert Tool",
     "author": "Takeshi",
     "version": (1, 0),
     "blender": (3, 0, 0),
     "location": "PROPERTIES > OBJECT DATA > ATTRIBUTES TO UVS",
-    "description": "Convert the Atrritutes in face corner to the UVs",
+    "description": "Convert the Attributes in face corner to the UVs",
     "warning": "It is not a stable version.",
     "doc_url": "",
-    "category": "Atrributes Convert Tool",
+    "category": "Attributes Convert Tool",
 }
 
 import bpy
 
 
-class Atrributes_Bake(bpy.types.Operator):
+class Attributes_Bake(bpy.types.Operator):
     """Bake acitive atrritute into active uv"""
-    bl_idname = "object.atrributes_bake"
+    bl_idname = "object.attributes_bake"
     bl_label = "Bake"
   
     @classmethod
@@ -40,10 +40,10 @@ class Atrributes_Bake(bpy.types.Operator):
         return {'FINISHED'}
 
     
-class Atrributes_To_UVs(bpy.types.Panel):
+class Attributes_To_UVs(bpy.types.Panel):
     """Bake the atrritutes in face corner into the UVs"""
-    bl_label = "Atrributes To UVs"
-    bl_idname = "Atrributes_To_UVs"
+    bl_label = "Attributes To UVs"
+    bl_idname = "Attributes_To_UVs"
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
     bl_context = "data"
@@ -66,19 +66,19 @@ class Atrributes_To_UVs(bpy.types.Panel):
         row.label(text = 'Destination UV: '+uv_name, icon='GROUP_UVS')
         
         row = layout.row()
-        row.operator("object.atrributes_bake")
+        row.operator("object.attributes_bake")
         
 def menu_func(self, context):
-    self.layout.operator(Atrributes_Bake.bl_idname, text=Atrributes_Bake.bl_label)
+    self.layout.operator(Attributes_Bake.bl_idname, text=Attributes_Bake.bl_label)
 
 def register():
-    bpy.utils.register_class(Atrributes_To_UVs)
-    bpy.utils.register_class(Atrributes_Bake)
+    bpy.utils.register_class(Attributes_To_UVs)
+    bpy.utils.register_class(Attributes_Bake)
     bpy.types.VIEW3D_MT_object.append(menu_func)
 
 def unregister():
-    bpy.utils.unregister_class(Atrributes_To_UVs)
-    bpy.utils.unregister_class(Atrributes_Bake)
+    bpy.utils.unregister_class(Attributes_To_UVs)
+    bpy.utils.unregister_class(Attributes_Bake)
     bpy.types.VIEW3D_MT_object.remove(menu_func)
 
 if __name__ == "__main__":
